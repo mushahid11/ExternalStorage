505000ddc0993a9629396e32e2a37673d4927b03
ARFA TECHNOLOGY <mushahidhussain1112@gmail.com>
2021-02-05 23:04:43 +0500

    Initial commit

---
+++ app/src/main/java/com/example/externalstorage/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.externalstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText etText;
+    TextView textView;
+    Button saveButton,readButton;
+
+    private String filename = "text.file";
+    private String filepath = "MyFileStorage";
+    File myExternalFile;
+    String myData = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etText = findViewById(R.id.myInputText);
+        textView = findViewById(R.id.response);
+
+
+        saveButton = findViewById(R.id.saveExternalStorage);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream fos = new FileOutputStream(myExternalFile);
+                    fos.write(etText.getText().toString().getBytes());
+                    etText.getText().clear();
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                etText.setText("");
+                textView.setText("text.file saved to External Storage...");
+            }
+        });
+
+        readButton = findViewById(R.id.getExternalStorage);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileInputStream fis = new FileInputStream(myExternalFile);
+                    DataInputStream in = new DataInputStream(fis);
+                    BufferedReader br =
+                            new BufferedReader(new InputStreamReader(in));
+                    String strLine;
+                    while ((strLine = br.readLine()) != null) {
+                        myData = myData + strLine;
+                    }
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                etText.setText(myData);
+                textView.setText("text.file data retrieved from Internal Storage...");
+            }
+        });
+
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            saveButton.setEnabled(false);
+        }
+        else {
+            myExternalFile = new File(getExternalFilesDir(filepath), filename);
+        }
+            }
+    private static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+
+        return false;
+    }
+    private static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+}
